9f5fe8099d9cb3e18b4e03b138d15939262ad6b8
RonSala <ron.sala@gmail.com>
2018-10-09 02:17:20 -0400

    pass 'lets a user view tweet edit form if they are logged in', 'does not let a user edit a tweet they did not create'

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,6 +17,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id/edit' do
+    if !logged_in?
+      redirect "/login"
+    else
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show'
+    else
+      redirect to '/login'
+    end
+  end
+
   post '/tweets' do
     if logged_in?
       if params[:content] == ""
@@ -35,13 +53,11 @@ class TweetsController < ApplicationController
     end
   end
 
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :'tweets/show'
+  patch '/tweets/:id' do
+    if !logged_in?
+      redirect "/login"
     else
-      redirect to '/login'
+      erb :'tweets/edit'
     end
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,6 @@ class UsersController < ApplicationController
 
 
   get "/users/:slug" do
-  # binding.pry
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
